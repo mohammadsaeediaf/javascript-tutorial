1ba277b754455c4c9e6215185dd9f05f3205ad91
VanGuard <alisaadati18000@gmail.com>
2023-10-21 20:40:57 +0330

    Add projects and file did before

---
+++ 11/3/script.js
@@ -0,0 +1,11 @@
+class person {
+  constructor(_name, _age, _height) {
+    this.name = _name;
+    this.age = _age;
+    this.height = _height;
+  }
+}
+
+const person1 = new person("amir", 18, 180);
+const person2 = new person("amir", 18, 180);
+console.log(person1);
