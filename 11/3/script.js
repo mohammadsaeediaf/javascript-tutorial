f91f2c21e2dbf367bec4e8ba9297795410cbdaa4
VanGuard <alisaadati18000@gmail.com>
2023-10-21 20:50:06 +0330

    Add rectangle class

--- 11/3/script.js
+++ 11/3/script.js
@@ -4,8 +4,21 @@ class person {
     this.age = _age;
     this.height = _height;
   }
+  getarea() {
+    return this.x;
+  }
 }
 
 const person1 = new person("amir", 18, 180);
 const person2 = new person("amir", 18, 180);
 console.log(person1);
+
+class rectangle {
+  constructor(_x, _y) {
+    this.x = _x;
+    this.y = _y;
+  }
+
+
+}
+
