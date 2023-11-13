0194143e4f1f42888a162a37116d0a5f35ab7edd
VanGuard <alisaadati18000@gmail.com>
2023-11-13 23:20:38 +0330

    add some change to modal

--- chapter 12/shoping page/script.js
+++ chapter 12/shoping page/script.js
@@ -1,6 +1,9 @@
 let shopIcon = document.querySelector(".shop-icon"),
   overlay = document.querySelector(".overlay"),
   closeModal = document.querySelector(".close-modal");
+
+let counter = 1;
+
 shopIcon.addEventListener("click", () => {
   overlay.classList.add("overlay-add");
 });
