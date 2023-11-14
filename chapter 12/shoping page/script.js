d267c9ac39af735dbe1bed4a4dfaad3b64a953da
VanGuard <alisaadati18000@gmail.com>
2023-11-14 23:29:04 +0330

    Write some style for modal-product

--- chapter 12/shoping page/script.js
+++ chapter 12/shoping page/script.js
@@ -1,8 +1,10 @@
 let shopIcon = document.querySelector(".shop-icon"),
   overlay = document.querySelector(".overlay"),
-  closeModal = document.querySelector(".close-modal");
-
-let counter = 1;
+  closeModal = document.querySelector(".close-modal"),
+  chevronUp = document.querySelector(".fa-chevron-up"),
+  chevronDown = document.querySelector(".fa-chevron-down"),
+  productCounter = document.querySelector(".counter"),
+  deleteItem = document.querySelector(".fa-trash");
 
 shopIcon.addEventListener("click", () => {
   overlay.classList.add("overlay-add");
@@ -10,4 +12,19 @@ shopIcon.addEventListener("click", () => {
 
 closeModal.addEventListener("click", () => {
   overlay.classList.remove("overlay-add");
+});
+
+let counter = 1;
+chevronUp.addEventListener("click", () => {
+  counter++;
+  if (counter >= 1) productCounter.textContent = counter;
+});
+
+chevronDown.addEventListener("click", () => {
+  counter--;
+  if (counter >= 1) productCounter.textContent = counter;
+});
+
+deleteItem.addEventListener("click", () => {
+  document.querySelector(".modal-product").style = "display: none";
 });
